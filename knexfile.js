86cc8a1f76ec877cab4fef64551a6a6e032e79a0
Aasa-Christian <aasachristian@yahoo.com>
2021-12-01 22:23:15 -0500

    config knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,19 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
 
-  staging: {
+  development: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'box_room',
+      user:     'postgres',
+      password: 'Mastat20'
     },
     pool: {
       min: 2,
