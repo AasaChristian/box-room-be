153b15761da8dbf25fcf3d91a536b6b2d69cd8f0
Aasa-Christian <aasachristian@yahoo.com>
2021-12-22 21:22:52 -0500

    cleaned trailer model

--- Models/trailers-model.js
+++ Models/trailers-model.js
@@ -1,32 +1,21 @@
 const db = require("../db-config.js");
 
 module.exports ={
-    // findBytrailerId,
     addTrailer,
-    // findLoggedExercises
+    getAll,
+    remove
 };
 
 function getAll(){
-    return db("trailers").select("*").orderBy('name')
-     
+    return db("trailers").select("*").orderBy('tagNumber')
 }
-// function findByuserId(userId){
-//     return db("logs")
-//     .where("logs.userId", userId)
-//     .join("regimen", "regimen.id", "logs.regimenId")
-//     .join("users as u", "u.id", "logs.userId")
-//     .join("exercises", "exercises.id", "regimen.exerciseId")
-//     .select("regimen.id as regimenID", "regimen.weight as regimenWeight", "regimen.name as regimenName", "logs.weight as LoggedWeight", "logs.reps as LoggedReps","logs.set as LoggedSet","*" ) 
-// }
 
 function addTrailer(trailer){
     return db("trailers").insert(trailer).returning("*")
 }
 
-// function findLoggedExercises(userId, exId){
-//     return db("logs")
-//     .where("logs.userId", userId)
-//     .join("regimen", "regimen.id", "logs.regimenId")
-//     .join("users as u", "u.id", "logs.userId")
-//     .join("exercises", "exercises.id", "regimen.exerciseId")
-// }
+function remove(id){
+    return db("trailers")
+    .where({id})
+    .del();
+}
