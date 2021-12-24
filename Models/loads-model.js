ba11b6e4993d2a1d8e90e1ebfc1d73c567347579
Aasa-Christian <aasachristian@yahoo.com>
2021-12-23 20:16:45 -0500

    created loads model

---
+++ Models/loads-model.js
@@ -0,0 +1,30 @@
+const db = require("../db-config.js");
+
+module.exports ={
+    addLoad,
+    remove,
+    updateLoad,
+    findById
+};
+
+function findById(id){
+    return db("loads").where({id})
+     
+}
+
+function addLoad(load){
+    return db("loads").insert(load).returning("*")
+}
+
+async  function updateLoad(load, id){
+    await db("loads")
+    .where({id})
+    .update(load)
+    return findById(id)  
+}
+
+function remove(id){
+    return db("loads")
+    .where({id})
+    .del();
+}
