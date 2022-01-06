4e8e1f3db3b62c8073de1272e0fa63ce4281ca32
Aasa-Christian <aasachristian@yahoo.com>
2022-01-05 22:41:16 -0500

    updated loads router, added findbyid endpoint

--- Routers/load-router.js
+++ Routers/load-router.js
@@ -9,8 +9,8 @@ const Loads = require('../Models/loads-model');
 
 
 router.get('/find/:id', (req, res) => {
-
-    Loads.getAll()
+const id = req.params.id
+    Loads.findById(id)
     .then(found => {
         if (!found){
         res.status(400).json({message:`no loads found`})  
@@ -60,58 +60,22 @@ res.status(201).json(updated);
  
 });
 
-// router.put('/unComplete/:link', (req, res) => {
 
-//   // console.log(req.body,"body")
-//   let { name, userId, exerciseId, sets, reps, weight, completion} = req.body;
-//   let link = req.params.link
-//   const updateObj = {
-//     name: name,
-//     link: link,
-//     userId: userId,
-//     exerciseId: exerciseId,
-//     sets: sets,
-//     reps, reps,
-//     weight: weight,
-//     completion: completion
-// }
-// Regimen.unComplete(updateObj, link)
+router.delete('/remove/:id', (req, res) => {
+    let id = req.params.id
 
-// .then(updated => {
-
-//     const uid = updated[0].userId
-//     // console.log(userId, "userId HERE")
-//     Regimen.findByuserId(uid)
-//     .then(found => {
-//       console.log("here")
-//         if (!found){
-//         res.status(400).json({message:`User has no regimen`})  
-//         }
-//         res.status(200).json(found)
-//         console.log("found")
-//     })
-//     .catch(err => {
-//         console.log(err,"catch")
-//     })
-
-// }).catch(err => res.status(500).json({message: "update unsuccesful"}), console.log("error here"))
- 
-// });
-
-// router.delete('/remove/:id', (req, res) => {
-//     let id = req.params.id
-
-//     Regimen.remove(id)
-//   .then(removed => {
-//       if (removed == 1){
-//         res.status(200).json({message: "regimen was removed"})
-//       } else {
-//         res.status(500).json({message: " error finding regimen"})
-//       }
+    Loads.remove(id)
+  .then(removed => {
+    console.log(removed, "removed")
+      // if (removed == 1){
+      //   res.status(200).json({message: "load was removed"})
+      // } else {
+      //   res.status(500).json({message: " error finding regimen"})
+      // }
       
-//   }).catch(err => res.status(500).json({message: "delete unsuccesful"}))
+  }).catch(err => res.status(500).json({message: "delete unsuccesful"}))
    
-//   });
+  });
 
 
 module.exports = router;
