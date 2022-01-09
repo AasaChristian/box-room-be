5faf8eafb97d94e25600ba5818d38eb96be8f613
Aasa-Christian <aasachristian@yahoo.com>
2022-01-09 16:23:14 -0500

    added name to trailer migration

--- migrations/20211202033046_trailers.js
+++ migrations/20211202033046_trailers.js
@@ -2,6 +2,8 @@ exports.up = function(knex) {
     return knex.schema.createTable("trailers", tbl => {
         tbl.increments();
         tbl.string('tagNumber').notNullable();
+        tbl.string('trailerName').defaultTo('N/A');
+
     })
 };
 exports.down = function(knex) {
