b333861c460e3bf6059c81f00d6fc04a0f44a042
Aasa-Christian <aasachristian@yahoo.com>
2022-01-10 23:08:05 -0500

    cleanup

--- Routers/trailer-router.js
+++ Routers/trailer-router.js
@@ -23,22 +23,6 @@ router.get('/all', (req, res) => {
   
 })
 
-// router.get('/find/:id', (req, res) => {
-//     let id = req.params.id
-//     // console.log(id, "id")
-
-//     Exercises.findById(id)
-//     .then(found => {
-//         if (!found){
-//         res.status(400).json({message:`exercises at id ${id} doesn't exist`})  
-//         }
-//         res.status(200).json(found)
-//     })
-//     .catch(err => {
-//         console.log(err,"catch")
-//     })
-    
-// })
 
 router.post('/add', (req, res) => {
   let trailer = req.body;
@@ -52,38 +36,6 @@ router.post('/add', (req, res) => {
       res.status(500).json(error);
     });
 });
-
-// router.put('/update/:id', (req, res) => {
-//   let { name, description, img } = req.body;
-//   let id = req.params.id
-//   const updateObj = {
-//     name: name,
-//     description: description,
-//     img: img
-// }
-// Exercises.updateExercise(updateObj, id)
-
-// .then(updated => {
-//     // console.log(updated, "updated")
-//     res.status(200).json({updated: updated})
-// }).catch(err => res.status(500).json({message: "update unsuccesful"}))
- 
-// });
-
-// router.delete('/remove/:id', (req, res) => {
-//     let id = req.params.id
-
-//   Exercises.remove(id)
-//   .then(removed => {
-//       if (removed == 1){
-//         res.status(200).json({message: "exercies was removed"})
-//       } else {
-//         res.status(500).json({message: " error finding exercise"})
-//       }
-      
-//   }).catch(err => res.status(500).json({message: "delete unsuccesful"}))
-   
-//   });
 
 
 module.exports = router;
