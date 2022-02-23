26847c2c0ca8e8c315b91cfa3c6859f6bc76aab0
Aasa-Christian <aasachristian@yahoo.com>
2022-02-23 12:35:28 -0500

    updated production connection to process env databaseUrl

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,6 @@
 
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
   development: {
     client: 'postgresql',
     connection: {
@@ -26,12 +19,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
