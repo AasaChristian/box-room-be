864a0333d5b3b0c318996a25272aa1faee6e79c5
Aasa-Christian <aasachristian@yahoo.com>
2022-03-13 23:32:41 -0400

    updated trailer router

--- Routers/trailer-router.js
+++ Routers/trailer-router.js
@@ -9,10 +9,12 @@ const Trailers = require('../Models/trailers-model');
 
 router.get('/all', (req, res) => {
 
+  console.log("trailers")
 
   Trailers.getAll()
   .then(found => {
       if (!found){
+        console.log(found, "trailers")
       res.status(400).json({message:`No trailers found`})  
       }
       res.status(200).json(found)
@@ -36,6 +38,23 @@ router.post('/add', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+
+router.delete('/remove/:id', (req, res) => {
+  let id = req.params.id
+
+Trailers.remove(id)
+.then(removed => {
+    if (removed == 1){
+      res.status(200).json({message: "trailer was removed"})
+    } else {
+      res.status(500).json({message: " error finding trailer"})
+    }
+    
+}).catch(err => res.status(500).json({message: "delete unsuccesful"}))
+ 
+});
+
 
 
 module.exports = router;
